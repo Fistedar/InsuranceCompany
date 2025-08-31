ce1822e4069af5246f8b693721f100738ecc171b
Fistedar <aidar741@gmail.com>
2025-08-31 22:42:43 +0300

    End BlackList app

--- v2/black-list/src/main/java/org/fistedar/black/list/rest/BlackListController.java
+++ v2/black-list/src/main/java/org/fistedar/black/list/rest/BlackListController.java
@@ -10,7 +10,7 @@ import org.fistedar.black.list.dto.BlackListRequest;
 import org.fistedar.black.list.dto.BlackListResponse;
 import org.fistedar.black.list.dto.PersonMapper;
 import org.fistedar.black.list.dto.ResponseMapper;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +25,9 @@ public class BlackListController {
     private final PersonMapper personMapper;
     private final ResponseMapper responseMapper;
 
-    @GetMapping("/")
+    @PostMapping(path = "/",
+            consumes = "application/json",
+            produces = "application/json")
     public BlackListResponse getBlackList(@RequestBody final BlackListRequest blackListRequest) {
         Stopwatch stopwatch = Stopwatch.createStarted();
         logger.logRequest(blackListRequest);
